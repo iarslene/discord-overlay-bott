f8efa8208cb55142f2ccbd14ab86fcf6d7d63d66
manii999 <maniha3105@gmail.com>
2025-06-26 18:50:07 -0400

    start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Install OCR engine
 apt-get update && apt-get install -y tesseract-ocr
-
-# Install system libraries needed by Playwright browsers
 playwright install-deps
-
-# Install Chromium for Playwright
 playwright install chromium
-
-# Run your bot
 python3 main.py
 
